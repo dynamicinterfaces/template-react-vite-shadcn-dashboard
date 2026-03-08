342e98b6896cbcd2089d1a2d44b51acb87ff7d20
Sandbox MCP <sandbox@dynamique.dev>
2026-03-08 20:42:53 +0000

    chore: add index preview

---
+++ src/features/financials/index.previews.tsx
@@ -0,0 +1,14 @@
+import { FinancialsDashboard } from './index'
+
+export default {
+  title: 'Features/FinancialsDashboard',
+  component: FinancialsDashboard,
+}
+
+export const Default = {
+  args: {},
+  parameters: {
+    layout: 'fullscreen',
+    route: '/financials',
+  },
+}
