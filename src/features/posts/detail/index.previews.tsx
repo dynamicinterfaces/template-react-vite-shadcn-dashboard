b36c65b537f908827383b7942002fee3766cd3a6
Sandbox MCP <sandbox@dynamique.dev>
2026-03-09 16:10:23 +0000

    chore: add index preview

--- src/features/posts/detail/index.previews.tsx
+++ src/features/posts/detail/index.previews.tsx
@@ -1,5 +1,5 @@
 import PostDetail from './index';
 
-const meta = { title: "PostDetail", component: PostDetail };
+const meta = { title: 'PostDetail', component: PostDetail };
 export default meta;
 export const Default = {};
