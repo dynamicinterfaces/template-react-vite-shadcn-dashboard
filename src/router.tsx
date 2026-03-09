59cef02b714c94ae180e0881da922e5b1d6eacdb
Dynamic Interfaces <sandbox@dyinterfaces.com>
2026-03-09 04:32:28 +0000

    [auto] checkpoint: src/router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -19,6 +19,7 @@ import DetailKanban from './features/kanban/detail'
 import EditKanban from './features/kanban/edit'
 import { Payments } from './features/payment'
 import FinancialsDashboard from './features/financials'
+import AccountingDashboard from './features/accounting'
 import Post from './features/posts'
 import PostDetail from './features/posts/detail'
 
