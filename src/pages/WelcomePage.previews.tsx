950e3d6f8f8b33489faa57ed03007f3dc813ff72
Sandbox MCP <sandbox@dynamique.dev>
2026-03-09 16:10:26 +0000

    chore: add WelcomePage preview

--- src/pages/WelcomePage.previews.tsx
+++ src/pages/WelcomePage.previews.tsx
@@ -1,5 +1,5 @@
 import WelcomePage from './WelcomePage';
 
-const meta = { title: "WelcomePage", component: WelcomePage };
+const meta = { title: 'WelcomePage', component: WelcomePage };
 export default meta;
 export const Default = {};
